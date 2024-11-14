e8194c10fc1a7d167b5b62d3e1bf7b3927d787d1
Moonwane <moonwane@qq.com>
2024-11-14 10:39:23 +0800

    init

---
+++ src/Demo.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import { Form as AForm } from '@arco-design/web-vue'
+import { reactive, ref } from 'vue'
+
+const formData = reactive({})
+const formRef = ref()
+</script>
+
+<template>
+    <!-- running type-check gets the following error -->
+    <!-- error TS2742: The inferred type of '__VLS_template' cannot be named without a reference to '.pnpm/scroll-into-view-if-needed@2.2.31/node_modules/scroll-into-view-if-needed'. This is likely not portable. A type annotation is necessary. -->
+    <slot></slot>
+    <a-form
+        ref="formRef"
+        :model="formData" />
+
+    <!-- following also gets error -->
+    <!-- <a-form
+        ref="formRef"
+        :model="formData">
+        <slot></slot>
+    </a-form> -->
+</template>
